09ce65b6dba922d1266c264d8db143d1fa739f68
jciterceros <jciterceros@yahoo.com>
2024-04-21 23:30:51 -0300

    feat: add Query Params find Product by Partial Name

--- src/main/java/com/jciterceros/DSCommerce/controllers/ProductController.java
+++ src/main/java/com/jciterceros/DSCommerce/controllers/ProductController.java
@@ -26,8 +26,9 @@ public class ProductController {
     }
 
     @GetMapping
-    public ResponseEntity<Page<ProductDTO>> findAll(Pageable pageable) {
-        Page<ProductDTO> dto = service.findAll(pageable);
+    public ResponseEntity<Page<ProductDTO>> findAll(
+            @RequestParam(name = "name", defaultValue = "") String name, Pageable pageable) {
+        Page<ProductDTO> dto = service.findAll(name, pageable);
         return ResponseEntity.ok(dto);
     }
 
@@ -46,7 +47,7 @@ public class ProductController {
     }
 
     @DeleteMapping(value = "/{id}")
-    public ResponseEntity<Void> update(@PathVariable long id) {
+    public ResponseEntity<Void> delete(@PathVariable long id) {
         service.delete(id);
         return ResponseEntity.noContent().build();
     }
